4a673a8c834a07af55bb6e4b9220ad8e3cbacda7
zakigoku00001 <zakigoku00001@gmail.com>
2025-07-27 00:01:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+ const express = require('express');
+const app = express();
+
+const links = [
+  'https://youtube.com',
+  'https://google.com'
+];
+
+let currentIndex = 0;
+
+app.get('/', (req, res) => {
+  const link = links[currentIndex];
+  currentIndex = (currentIndex + 1) % links.length;
+  res.redirect(link);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
