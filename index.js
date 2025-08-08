78865045546a976afa5c840a73b7e9d3517b58ac
zakigoku00001 <zakigoku00001@gmail.com>
2025-08-09 00:47:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const links = [
   'https://afflat3d2.com/trk/lnk/01A64732-5ECB-4F5A-A67E-A974C3441DA1/?o=26175&c=918277&a=743272&k=AE26A5D871F142E64B69657663F9EDE3&l=27558',
   'https://afflat3d2.com/trk/lnk/01A64732-5ECB-4F5A-A67E-A974C3441DA1/?o=26175&c=918277&a=743272&k=AE26A5D871F142E64B69657663F9EDE3&l=27558',
   'https://afflat3d2.com/trk/lnk/01A64732-5ECB-4F5A-A67E-A974C3441DA1/?o=26175&c=918277&a=743272&k=AE26A5D871F142E64B69657663F9EDE3&l=27558',
-  'https://afflat3e1.com/trk/lnk/6EF68319-E3B5-4F1B-97B2-F26EC53230CB/?o=6365&c=918277&a=752842&k=92AC6329A709B55F5AE017721B6470BE&l=5077',
-  'https://afflat3e1.com/trk/lnk/CDF22B93-9B70-4B2A-88FE-49DF6698DCA8/?o=6365&c=918277&a=753433&k=8B441691ABE8760EA83A84AA968F3D8B&l=5077'
+  'https://rewarrdsgiant.com/aff_c?offer_id=2593&aff_id=58197'
 ];
 
 let currentIndex = 0;
@@ -19,5 +18,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
